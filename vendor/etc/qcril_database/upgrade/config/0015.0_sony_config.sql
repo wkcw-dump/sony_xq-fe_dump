de3a95a707add70a37362ee0cdcd29d4d1f17744
Byben <heybyben@gmail.com>
2026-09-12 10:41:19 +0700

    Add vendor for XQ-FE54_EEA-user 16 72.1.A.2.198 072001A002019801752368871 release-keys
    Change-Id: If8cee29d8fdc168bb46d8473bff05be05b7d64f9
    Signed-off-by: Byben <heybyben@gmail.com>

---
+++ vendor/etc/qcril_database/upgrade/config/0015.0_sony_config.sql
@@ -0,0 +1,7 @@
+/*
+Copyright 2024 Sony Corporation
+*/
+
+UPDATE qcril_properties_table SET def_val="1" WHERE property="persist.vendor.radio.manual_nw_rej_ct";
+UPDATE qcril_properties_table SET def_val="19" WHERE property="persist.vendor.radio.mt_sms_ack";
+UPDATE qcril_properties_table SET def_val="true" WHERE property="persist.vendor.radio.unicode_op_names";
